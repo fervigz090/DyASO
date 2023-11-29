dcc1fac9b0feb4f2d386504a18c39e4c1ccab78a
Parallels <parallels@kali.localdomain>
2023-11-29 12:49:31 +0100

    finalizado

--- Plantilla_Trabajo_DyASO/DyASO_PED1_Fernandez_Rodriguez_Ivan/Trabajo1/Demonio.sh
+++ Plantilla_Trabajo_DyASO/DyASO_PED1_Fernandez_Rodriguez_Ivan/Trabajo1/Demonio.sh
@@ -1,133 +1,60 @@
 #!/bin/bash
 
-#Demonio Dummie, tenéis que completarlo para que haga algo
+#Directorio donde se ubican los archivos compartidos
+DIR_COMPARTIDO="$(dirname "$0")"  #Directorio del script Demonio.sh
 
 #Bucle mientras que no llegue el apocalipsis
-#   -Espera un segundo
-#   -Lee las listas y revive los procesos cuando sea necaario dejando entradas en la biblia
-#   -Puede usar todos los ficheros temporales que quiera pero luego en el Apocalipsis hay que borrarlos
-#   -Hay que usar un lock para no acceder a las listas a la vez que Fausto
-#   -Ojo al cerrar los proceos, hay que terminar el arbol completo no sólo uno de ellos
-#Fin bucle
-
-#Apocalipsis: termino todos los procesos y limpio todo dejando sólo Fausto, el Demonio y la Biblia
-
-time=''
-# FUNCIONES
-
-# Comprobación de existencia de un proceso por PID
-proceso_en_ejecucion() {
-    local pid="$1"
-    if ps -p "$pid" > /dev/null; then
-        return 0  # El proceso está en ejecución
-    else
-        return 1  # El proceso no está en ejecución
-    fi
-}
-
-# Resurrección de proceso
-resucitar() {
-    local pid="$1"
-    local comando=$(echo "$2" | sed "s/'//g")
-    # Elimina el proceso hijo
-    pgrep -P "$pid" | xargs kill -9
-    # Elimina el proceso padre con senal KILL
-    kill -9 "$pid"
-    # Elimina el proceso de la lista, ya que el nuevo no tiene el mismo pid ni ppid
-    sed -i "/$pid/d" procesos_servicio
-	# Lanza de nuevo el comando como hijo de un proceso bash
-	bash -c "$comando" &
-	# Obtiene el pid del padre
-	pid=$!
-	# Guarda el nuevo proceso en la lista
-	echo "$pid $comando" >> procesos_servicio
-	time=$(date +%H:%M:%S)
-    echo $time: "El proceso $1 resucita con pid $pid" >> Biblia.txt
-
-}
-# Lanzamiento procesos periodicos
-lanzamiento_periodico() {
-    while read -r t_actual T ppid cmd; do
-   	    modulo=$((t_actual % T))
-		echo "$modulo" >> probas.txt
-        if [ "$modulo" -eq 0 ]; then
-            # Ejecutar el comando y mantener el proceso en lista si es necesario
-			echo "$cmd" >> probas.txt
-            eval "$cmd"
-            t_actual=$((t_actual + 1))
-        else
-            # Incrementar el tiempo y mantener el proceso en lista si es necesario
-            t_actual=$((t_actual + 1))
-        fi
-
-        # Actualizar la entrada en el archivo 'procesos_periodicos'
-        sed -i "/\b$ppid\b/d" procesos_periodicos
-        echo "$t_actual $T $ppid $cmd" >> procesos_periodicos
-    done < "procesos_periodicos"
-}
-
-
-# Comienza el proceso Demonio
 while true; do
     sleep 1  # Espera 1 segundo
 
     # Comprobación del apocalipsis
-    if [ -e Apocalipsis ]; then
+    if [ -e "$DIR_COMPARTIDO/Apocalipsis" ]; then
         # Eliminación de todos los procesos de las listas
-		time=$(date +%H:%M:%S)
-        echo "$time: -------------Apocalipsis-------------" >> Biblia.txt
+        time=$(date +%H:%M:%S)
+        echo "$time: -------------Apocalipsis-------------" >> "$DIR_COMPARTIDO/Biblia.txt"
 
-		# Procesos servicio
-      	pServicio="procesos_servicio"
-		while IFS=' ' read -r ppidServ comandoPS_completo; do
-        	comandoPS=$(echo "$comandoPS_completo" | awk '{print $1}' | sed "s/'//") # Devuelve la palabra reservada del comando sin comillas
-			pkill "$comandoPS"
-			echo $time:" El proceso '$comandoPS' ha terminado" >> Biblia.txt
-		done < "$pServicio"
+        # Eliminacion Procesos servicio
+        pServicio="$DIR_COMPARTIDO/procesos_servicio"
+        while IFS=' ' read -r ppidServ comandoPS_completo; do
+            comandoPS=$(echo "$comandoPS_completo" | awk '{print $1}' | sed "s/'//") # Devuelve la palabra reservada del comando sin comillas
+            pkill "$comandoPS"
+            echo "$time: El proceso '$comandoPS' ha terminado" >> "$DIR_COMPARTIDO/Biblia.txt"
+        done < "$pServicio"
 
-		# Procesos normales
-		procesos="procesos"
-		while IFS=' ' read -r ppid comandoP_completo; do
-			kill ppid
-			echo $time:" El proceso $comandoP_completo ha terminado" >> Biblia.txt
-		done < "$procesos"
+        # Eliminacion Procesos normales
+        procesos="$DIR_COMPARTIDO/procesos"
+        while IFS=' ' read -r ppid comandoP_completo; do
+            kill ppid
+            echo "$time: El proceso $comandoP_completo ha terminado" >> "$DIR_COMPARTIDO/Biblia.txt"
+        done < "$procesos"
 
-		# Procesos periodicos
-		pPeriodicos="procesos_periodicos"
-		while IFS=' ' read -r T_arranque T ppid comando; do
-			kill ppid
-			pkill Fausto.sh
-			echo $time:" El proceso $comando ha terminado" >> Biblia.txt
-		done < "$pPeriodicos"
+        # Eliminacion Procesos periodicos
+        pPeriodicos="$DIR_COMPARTIDO/procesos_periodicos"
+        while IFS=' ' read -r T_arranque T ppid comando; do
+            kill ppid
+            pkill Fausto.sh
+            echo "$time: El proceso $comando ha terminado" >> "$DIR_COMPARTIDO/Biblia.txt"
+        done < "$pPeriodicos"
 
-		find -type f \( ! -name "*.sh" -a ! -name "Biblia.txt" -a ! -name "test*" \) -exec rm -f {} \;
-		find -type d -exec rm -r -f;
-		rm -r -f infierno
-		pkill sleep
-		kill $$ # Demonio.sh se elimina a si mismo
+		# Eliminacion de ficheros
+        find "$DIR_COMPARTIDO" -type f \( ! -name "*.sh" -a ! -name "Biblia.txt" -a ! -name "test*" \) -exec rm -f {} \;
+        find "$DIR_COMPARTIDO" -type d -exec rm -r -f {} \;
+        rm -r -f "$DIR_COMPARTIDO/infierno"
+        pkill sleep
+
+		# El proceso Demonio se elimina a si mismo
+        kill $$
     fi
 
     # Comprobación procesos-servicio
     while IFS=' ' read -r pidServ comando; do
         if ! proceso_en_ejecucion "$pidServ"; then
             resucitar "$pidServ" "$comando"
-			break
+            break
         fi
-    done < "procesos_servicio"
+    done < "$DIR_COMPARTIDO/procesos_servicio"
 
-	# Comprobacion procesos-periodicos
-	lanzamiento_periodico &
+    # Comprobación procesos-periodicos
+    lanzamiento_periodico &
 done
-
-
-
-
-
-
-
-
-
-
-
-
 
